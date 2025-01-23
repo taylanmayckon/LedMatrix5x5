7d4c11cd7764593138c6b145908be0f4cc5f200c
Lucasferreira08 <lucasferreira210802@gmail.com>
2025-01-23 14:28:51 -0300

    Adicionando animação da tecla D

--- LedMatrix5x5.c
+++ LedMatrix5x5.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "libs/teclado_matricial/teclado_matricial.h"
+#include "tecla_d/animacao.h"
 
 
 int main()
 {
     stdio_init_all();
 
+    set_rows_output();
+    set_cols_input();
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        char tecla = detect_button(); // Armazena a tecla detectada
+        printf("Tecla pressionada: %c\n", tecla);
+
+        if (tecla == 'D' || tecla == '4') {
+            main_animacao(); // Aciona o buzzer
+        }
+
+        sleep_ms(50); // Delay para evitar múltiplas leituras rápidas
     }
 }
